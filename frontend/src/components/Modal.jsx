6ad9088a363d589a1a6762df008d4d682dc82559
Maeva Fauvel <mae.fauvel@gmail.com>
2024-01-17 16:12:38 +0100

    bug fix

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/prop-types */
 import "../styles/Modal.scss";
-import Spline from '@splinetool/react-spline';
-import Remove from '../assets/remove.png';
+import Spline from "@splinetool/react-spline";
+import Remove from "../assets/remove.png";
 
-export default function Modal({ splineUrl, onClose}) {
+export default function Modal({ splineUrl, onClose }) {
   const handleContentClick = (element) => {
     element.stopPropagation();
   };
@@ -11,11 +12,16 @@ export default function Modal({ splineUrl, onClose}) {
   return (
     <div className="modal">
       <div className="overlay" onClick={onClose}>
-        <img src={Remove} alt="close" className="close-modal" onClick={onClose}/>
+        <img
+          src={Remove}
+          alt="close"
+          className="close-modal"
+          onClick={onClose}
+        />
         <div className="modal-content" onClick={handleContentClick}>
           <div className="modele3D">
             <Spline scene={splineUrl} />
-            </div>
+          </div>
         </div>
       </div>
     </div>
