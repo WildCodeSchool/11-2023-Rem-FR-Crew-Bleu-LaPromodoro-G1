a8e854b63e88418fee522d5925454eeb196ae65f
Gwenaelbegot <150467591+Gwenaelbegot@users.noreply.github.com>
2024-01-17 16:23:08 +0100

    Revert "008 bulle de naration"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,44 +4,11 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
-import Niveau1 from "./pages/Niveau1/components/Niveau1";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/menu-principal",
-    // element: <Accueil />,
-  },
-  {
-    path: "/niveau1",
-    element: <Niveau1 />,
-  },
-  {
-    path: "/niveau2",
-    // element: </>
-  },
-  {
-    path: "/niveau3",
-    // element: </>
-  },
-  {
-    path: "/niveau4",
-    // element: </>
-  },
-  {
-    path: "/niveau5",
-    // element: </>
-  },
-  {
-    path: "/cimetiere",
-    // element: </>
-  },
-  {
-    path: "/asile",
-    // element: </>
   },
 ]);
 
